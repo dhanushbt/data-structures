08978267db1f68a3fadc2673d38799d20f83dc1c
Dhanush <96831879+dhanushbt@users.noreply.github.com>
2022-02-26 15:21:40 +0530

    Create valid_paranthesis.py

---
+++ valid_paranthesis.py
@@ -0,0 +1,23 @@
+#to check the string has same opening and closing bracket
+#we match the similar brackets and remove it from the top of stack
+Class solution:
+  def isvalid(self,s):
+    stack=[]
+    #hash map to map closing with opening brackets
+    closeToOpen={")":"(","}":"{","[":"]"}
+    #search over string->to build stack
+    for char in s:
+      #if the char in closing bracket
+      if char in closeToOpen:
+        #check stack is not empty and value of top is matching opening bracket
+        if stack and stack[-1]==closeToOpen[c]:
+          stack.pop()
+        else:
+          return False
+     else:
+      #append open parentesis
+       stack.append(c)
+        
+  
+  return True if not stack else false
+    
